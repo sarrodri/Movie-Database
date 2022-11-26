4ae1692e748552dd7d93dac8e1caeeedb7a55517
sarrodri <sarahesther00@yahoo.com>
2022-11-26 10:55:58 -0600

    API key start

--- MovieDB-Sarah.js
+++ MovieDB-Sarah.js
@@ -1,1 +1,25 @@
-// comment number 2
+// API
+
+const API_KEY = " ";
+const BAS_URL = " ";
+const API_URL = BAS_URL + " " + API_KEY;
+
+getMovies(API_URL);
+
+function getMovies(url){
+
+    fetch(url).then(res => res.json()).then(data => {
+        
+        showMovies(data.results);
+    })
+
+}
+
+function showMovies(data) {
+
+    data.forEach(movie =. {
+        const movieE1 = document.createElement('div');
+        movieE1.classList.add('movie');
+        movieE1.innerHTML = "paste from index, see 30:00 on YT video"
+    })
+}
